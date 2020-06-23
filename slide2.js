e6f7e6b19da8ad341185430c53f1c224d4188adf
saeyoon <ylyy44@naver.com>
2020-06-23 16:13:37 +0900

    autoplay, pause 버튼 적용완료

--- slide2.js
+++ slide2.js
@@ -25,18 +25,22 @@ container.style.width = containerWidht + 'px';
 // 클론아이템때문에 첫번째는 슬라이드의 맨마지막 아이템이 나타나는 이슈 해결
 container.style.transform = 'translateX(' + (-itemWidth * count) + 'px)';
 
-// next, prev버튼
+// next, prev, auto 버튼
 const btnBox = document.querySelector('#slide_button');
 const prevBtn = document.createElement('button');
 const nextBtn = document.createElement('button');
+const autoBtn = document.createElement('button');
 
 prevBtn.setAttribute('id', 'prev_button');
 nextBtn.setAttribute('id', 'next_button');
+autoBtn.setAttribute('id', 'auto_button');
 btnBox.append(prevBtn);
 btnBox.append(nextBtn);
+btnBox.append(autoBtn);
 
 nextBtn.addEventListener('click', nextBtnHandler);
 prevBtn.addEventListener('click', prevBtnHandler);
+autoBtn.addEventListener('click', autoBtnHandler);
 
 container.addEventListener('transitionend', moveFirstItem);
 container.addEventListener('transitionend', moveLastItem);
@@ -60,7 +64,6 @@ function nextBtnHandler() {
         curDot = dots[0];
         dots[0].classList.add('dot_active');
     }
-    console.log(curDot)
     
     container.style.transform = 'translateX(' + (-itemWidth * count) + 'px)'
 }
@@ -82,10 +85,24 @@ function prevBtnHandler() {
         curDot = dots[dots.length - 1];
         dots[dots.length - 1].classList.add('dot_active');
     }
-    console.log(curDot)
 
     container.style.transform = 'translateX(' + (-itemWidth * count) + 'px)'
 }
+
+//auto 버튼 클릭시
+var pause = null;
+
+function autoBtnHandler() {
+    this.classList.toggle('on');
+
+    if(pause != null){   //인터벌 담은 변수가 null이 아니면 ex) 현재 인터벌 중이면
+       clearInterval(pause);   // 인터벌을 끝내라
+       pause = null;    //변수 초기화
+       return false;    // 함수나가기
+    }
+
+    pause = setInterval(nextBtnHandler, 3000); // 인터벌담은 변수가 null이면 반복실행해라
+};
 
 // 무한루프
 function moveFirstItem() {
@@ -132,3 +149,5 @@ Array.prototype.forEach.call(dots, function(dot){
         container.style.transform = 'translateX(' + (-itemWidth * count) + 'px)'
     });
 });
+
+//auto play
